2bced167c9a54971f75c98de7077ad3d85d73d7c
patryk wysocki <c00213855@itcarlow.ie>
2017-12-07 12:56:28 +0000

    more playing with primitives

--- gameplay-programming-i-lab-05/SFMLOpenGL/Game.cpp
+++ gameplay-programming-i-lab-05/SFMLOpenGL/Game.cpp
@@ -36,7 +36,7 @@ void Game::initialize()
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
 	glMatrixMode(GL_PROJECTION); 
 	glLoadIdentity(); 
-	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0); 
+	gluPerspective(100.0, window.getSize().x / window.getSize().y, 1.0, 500.0); 
 	glMatrixMode(GL_MODELVIEW);
 	//
 }
@@ -49,7 +49,8 @@ void Game::update()
 void Game::draw()
 {
 	cout << "Draw up" << endl;
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//TRIANGLE
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	//glBegin(GL_TRIANGLES); 
 	//{
 	//	glVertex3f(0.0, 2.0, -5.0); 
@@ -57,14 +58,16 @@ void Game::draw()
 	//	glVertex3f(2.0, -2.0, -5.0); 
 	//} 
 	//glEnd(); 
+	//POINTS
 	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	//glBegin(GL_POINTS);
 	//{
-	//	glVertex3f(0.0, 2.0, -5.0);
-	//	glVertex3f(-2.0, -2.0, -5.0);
-	//	glVertex3f(2.0, -2.0, -5.0);
+	//	glVertex3f(0.0, 2.0, -5.0); //TOP
+	//	glVertex3f(-2.0, -2.0, -5.0); //LEFT
+	//	glVertex3f(2.0, -2.0, -5.0);//RIGHT
 	//}
 	//glEnd();
+	//LINES
 	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	//glBegin(GL_LINES);
 	//{
@@ -74,11 +77,64 @@ void Game::draw()
 
 	////}
 	//glEnd();
-	glBegin(GL_LINE_STRIP);
+
+	//LINESTRIP
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//glBegin(GL_LINE_STRIP);
+	//{
+
+	//	glVertex3f(-4.00, 0.00, -5.00);
+	//	glVertex3f(-3.00, 2.00, -5.00);
+	//	glVertex3f(-2.00, 0.00, -5.00);
+	//	glVertex3f(-1.00, 2.00, -5.00);
+	//	glVertex3f(0.0, 0.00, -5.00);
+	//	glVertex3f(1.00, 2.00, -5.00);
+	//	glVertex3f(2.00, 0.00, -5.00);
+	//	glVertex3f(3.00, 2.00, -5.00);
+	//	glVertex3f(4.00, 0.00, -5.00);
+	//}
+	//glEnd();
+
+	//LINE LOOP
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//glBegin(GL_LINE_LOOP);
+	//{
+	//		glVertex3f(0.00, 2.00, -5.00);
+	//		glVertex3f(-3.00, -2.00, -1.00);
+	//		glVertex3f(-2.00, -5.00, -2.00);
+	//		glVertex3f(0.0, 0.00, -5.00);
+	//		glVertex3f(1.00, 2.00, -2.00);
+	//		glVertex3f(2.00, 0.00, -3.00);
+	//		glVertex3f(3.00, 2.00, -3.00);
+	//		glVertex3f(4.00, 0.00, -1.00);
+
+
+
+	//}
+	//glEnd();
+
+	//TRIANGLESTRIP
+	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//glBegin(GL_TRIANGLE_STRIP);
+	//{
+	//	glVertex3f(0.0, 2.0, -5.0); //TOP
+	//	glVertex3f(-2.0, -2.0, -5.0); //LEFT
+	//	glVertex3f(2.0, -2.0, -5.0);//RIGHT
+
+	//	glVertex3f(0.0, 2.0, -5.0); //TOP
+	//	glVertex3f(1.00, 1.00, -1.00);
+	//	glVertex3f(2.0, -2.0, -5.0);//RIGHT
+
+	//	glVertex3f(2.0, -2.0, -5.0);//RIGHT
+	//	glVertex3f(1.00, 1.00, -1.00);
+	//	glVertex3f(-1.0, 2.0, -3.0); //TOP
+	//}
+	//glEnd();
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glBegin(GL_TRIANGLE_FAN);
 	{
-			glVertex3f(0.0, 2.0, -5.0);
-			glVertex3f(-2.0, -2.0, -5.0);
-			glVertex3f(2.0, -2.0, -5.0);
+
 	}
 	glEnd();
 	window.display();
