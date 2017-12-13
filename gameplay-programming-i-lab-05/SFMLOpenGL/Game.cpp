7bc6aa0075e06a11043c2ba8e41988abc2d4a138
(Student) - Patryk Wysocki <c00213855@ITCARLOW.IE>
2017-12-13 15:13:26 +0000

    part2

--- gameplay-programming-i-lab-05/SFMLOpenGL/Game.cpp
+++ gameplay-programming-i-lab-05/SFMLOpenGL/Game.cpp
@@ -1,8 +1,19 @@
 #include <Game.h>
 
-Game::Game() : window(VideoMode(800, 600), "OpenGL")
+// // Uncomment for Part 2
+// // ********************
+bool flip = false;
+int current = 1;
+// // ********************
+
+  Game::Game() : window(VideoMode(800, 600), "OpenGL"), primatives(2)
 {
+	// // Uncomment for Part 2
+	// // ********************
+	 index = glGenLists(primatives);
+	// // ********************
 }
+
 
 Game::~Game() {}
 
@@ -33,111 +44,100 @@ void Game::run()
 void Game::initialize()
 {
 	isRunning = true;
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); 
-	glMatrixMode(GL_PROJECTION); 
-	glLoadIdentity(); 
-	gluPerspective(100.0, window.getSize().x / window.getSize().y, 1.0, 500.0); 
+
+	// // Uncomment for Part 2
+	// // ********************
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
 	glMatrixMode(GL_MODELVIEW);
-	//
+
+
+	glNewList(index, GL_COMPILE);
+	glBegin(GL_TRIANGLES);
+	{
+		glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex3f(0.0, 2.0, -5.0);
+		glVertex3f(-2.0, -2.0, -5.0);
+		glVertex3f(2.0, -2.0, -5.0);
+	}
+	glEnd();
+	glEndList();
+
+	glNewList(index + 1, GL_COMPILE);
+	glBegin(GL_TRIANGLES);
+	{
+		glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex3f(0.2, 0.0, -2.0);
+		glVertex3f(-2.0, -2.0, -2.0);
+		glVertex3f(2.0, -2.0, -2.0);
+	}
+	glEnd();
+	glEndList();
+
+	// // Uncomment for Part 2
+	// // ********************
 }
 
 void Game::update()
 {
+
+	// // Uncomment for Part 2
+	// // ********************
+	elapsed = clock.getElapsedTime();
+
+	if (elapsed.asSeconds() >= 1.0f)
+	{
+		clock.restart();
+
+		if (!flip)
+		{
+			flip = true;
+			current++;
+			if (current > primatives)
+			{
+				current = 1;
+			}
+		}
+		else
+			flip = false;
+	}
+
+	if (flip)
+	{
+		rotationAngle += 0.005f;
+
+		if (rotationAngle > 360.0f)
+		{
+			rotationAngle -= 360.0f;
+		}
+	}
+	// // ********************
+
 	cout << "Update up" << endl;
 }
 
 void Game::draw()
 {
+
+	// // Uncomment for Part 2
+	// // ********************
 	cout << "Draw up" << endl;
-	//TRIANGLE
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glBegin(GL_TRIANGLES); 
-	//{
-	//	glVertex3f(0.0, 2.0, -5.0); 
-	//	glVertex3f(-2.0, -2.0, -5.0); 
-	//	glVertex3f(2.0, -2.0, -5.0); 
-	//} 
-	//glEnd(); 
-	//POINTS
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glBegin(GL_POINTS);
-	//{
-	//	glVertex3f(0.0, 2.0, -5.0); //TOP
-	//	glVertex3f(-2.0, -2.0, -5.0); //LEFT
-	//	glVertex3f(2.0, -2.0, -5.0);//RIGHT
-	//}
-	//glEnd();
-	//LINES
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glBegin(GL_LINES);
-	//{
-	//	glVertex3f(0.0, 2.0, -5.0);
-	//	glVertex3f(-2.0, -2.0, -5.0);
-	//	glVertex3f(2.0, -2.0, -5.0);
-
-	////}
-	//glEnd();
-
-	//LINESTRIP
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glBegin(GL_LINE_STRIP);
-	//{
-
-	//	glVertex3f(-4.00, 0.00, -5.00);
-	//	glVertex3f(-3.00, 2.00, -5.00);
-	//	glVertex3f(-2.00, 0.00, -5.00);
-	//	glVertex3f(-1.00, 2.00, -5.00);
-	//	glVertex3f(0.0, 0.00, -5.00);
-	//	glVertex3f(1.00, 2.00, -5.00);
-	//	glVertex3f(2.00, 0.00, -5.00);
-	//	glVertex3f(3.00, 2.00, -5.00);
-	//	glVertex3f(4.00, 0.00, -5.00);
-	//}
-	//glEnd();
-
-	//LINE LOOP
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glBegin(GL_LINE_LOOP);
-	//{
-	//		glVertex3f(0.00, 2.00, -5.00);
-	//		glVertex3f(-3.00, -2.00, -1.00);
-	//		glVertex3f(-2.00, -5.00, -2.00);
-	//		glVertex3f(0.0, 0.00, -5.00);
-	//		glVertex3f(1.00, 2.00, -2.00);
-	//		glVertex3f(2.00, 0.00, -3.00);
-	//		glVertex3f(3.00, 2.00, -3.00);
-	//		glVertex3f(4.00, 0.00, -1.00);
-
-
-
-	//}
-	//glEnd();
-
-	//TRIANGLESTRIP
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glBegin(GL_TRIANGLE_STRIP);
-	//{
-	//	glVertex3f(0.0, 2.0, -5.0); //TOP
-	//	glVertex3f(-2.0, -2.0, -5.0); //LEFT
-	//	glVertex3f(2.0, -2.0, -5.0);//RIGHT
-
-	//	glVertex3f(0.0, 2.0, -5.0); //TOP
-	//	glVertex3f(1.00, 1.00, -1.00);
-	//	glVertex3f(2.0, -2.0, -5.0);//RIGHT
-
-	//	glVertex3f(2.0, -2.0, -5.0);//RIGHT
-	//	glVertex3f(1.00, 1.00, -1.00);
-	//	glVertex3f(-1.0, 2.0, -3.0); //TOP
-	//}
-	//glEnd();
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBegin(GL_TRIANGLE_FAN);
-	{
 
-	}
-	glEnd();
+	//Investigate Here!!!!!
+	glRotatef(rotationAngle, 0.0f, 0.0f, 1.0f);
+	glTranslatef(1.0f, 0.0f, 0.0f);
+	glScalef(1.0f, 1.0f, 1.0f);
+	cout << "Drawing Primative " << current << endl;
+	glCallList(current);
+	// // Uncomment for Part 2
+	// // ********************
+
 	window.display();
+
 }
 
 void Game::unload()
--- gameplay-programming-i-lab-05/SFMLOpenGL/Game.h
+++ gameplay-programming-i-lab-05/SFMLOpenGL/Game.h
@@ -23,12 +23,12 @@ private:
 
 	// // Uncomment for Part 2
 	// // ********************
-	// const int primatives;
+	 const int primatives;
 
-	//GLuint index;
-	//Clock clock;
-	//Time elapsed;
+	GLuint index;
+	Clock clock;
+	Time elapsed;
 
-	//float rotationAngle = 0.0f;
+	float rotationAngle = 0.0f;
 	// // ********************
 };
